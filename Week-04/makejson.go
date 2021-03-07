f04acdc4c1667eaf83356b1e727ad76383f967f2
Mohitp98 <mohit.patil@gslab.com>
2021-03-08 00:13:32 +0530

    added question

--- Week-04/makejson.go
+++ Week-04/makejson.go
@@ -1,3 +1,13 @@
+/*
+QUESTION:
+
+Write a program which prompts the user to first enter a name, and then enter an address.
+Your program should create a map and add the name and address to the map using the keys “name” and “address”,
+respectively. Your program should use Marshal() to create a JSON object from the map, and then your program
+should print the JSON object.
+
+*/
+
 package main
 
 import (
